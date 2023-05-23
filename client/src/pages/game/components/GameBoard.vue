12cc0c9396d5e57de3230a72f653d174f191ff9e
Marcin Radwan <marcin.radwan08@gmail.com>
2023-05-23 19:11:45 +0200

    feat: simple information about players count

--- client/src/pages/game/components/GameBoard.vue
+++ client/src/pages/game/components/GameBoard.vue
@@ -1,15 +1,18 @@
 <template>
-    <GameBoardLoader v-if="isLoadingState"
-                     :progress-value="loadingProgress"
-                     :loading-stage="loadingStage"></GameBoardLoader>
-    <div class="game-board__overlay">
-        <div class="game-board__wrapper">
-            <canvas ref="gameBoardRef"
-                    :width="GAME_CONFIG.width"
-                    :height="GAME_CONFIG.height"
-                    class="game-board__canvas"></canvas>
-        </div>
+  <GameBoardLoader v-if="isLoadingState"
+                   :progress-value="loadingProgress"
+                   :loading-stage="loadingStage"></GameBoardLoader>
+  <div class="game-board__overlay">
+    <div class="game-board-header-top__wrapper">
+      Graczy: {{ playersCount }}
     </div>
+    <div class="game-board__wrapper">
+      <canvas ref="gameBoardRef"
+              :width="GAME_CONFIG.width"
+              :height="GAME_CONFIG.height"
+              class="game-board__canvas"></canvas>
+    </div>
+  </div>
 </template>
 <script lang="ts">
 import {ref} from "vue";
@@ -25,128 +28,135 @@ import {MainCharacter} from "@/models/main.character.class";
 let socketInstance: any;
 
 export default {
-    name: 'GameBoard',
-    components: {
-        GameBoardLoader
-    },
-    mounted() {
-        socketInstance = getSocketClientInstance(this);
-    },
-    setup() {
-        let gameContext: any = null;
-        const gameBoardRef = ref(null);
-        const isLoadingState = ref(true);
-        const loadingProgress = ref(0);
-        const loadingStage = ref(0);
-        let characterPlaceholderImg: HTMLImageElement;
-        let character: MainCharacter;
-        let localization: Localization;
-        let bgImage: HTMLImageElement;
-        // const graphicsMap = new Map<string, HTMLImageElement>();
-        const playersMap = new Map<string, Character>();
+  name: 'GameBoard',
+  components: {
+    GameBoardLoader
+  },
+  mounted() {
+    socketInstance = getSocketClientInstance(this);
+  },
+  setup() {
+    let gameContext: any = null;
+    const gameBoardRef = ref(null);
+    const isLoadingState = ref(true);
+    const loadingProgress = ref(0);
+    const loadingStage = ref(0);
+    const playersCount = ref(1);
 
-        const onInitialDataLoaded = (data: any) => loadGame(data);
-        const onPlayersMovement = (data: any) => {
-            if (!data || !data.players) {
-                return;
-            }
+    let characterPlaceholderImg: HTMLImageElement;
+    let character: MainCharacter;
+    let localization: Localization;
+    let bgImage: HTMLImageElement;
+    // const graphicsMap = new Map<string, HTMLImageElement>();
+    const playersMap = new Map<string, Character>();
 
-            data.players.forEach((player: any) => {
-                if (playersMap.has(player.summonerId)) {
-                    let playerData = playersMap.get(player.summonerId);
-                    if (!playerData) {
-                        return;
-                    }
-                }
-            });
-        };
-        const onAnotherPlayerConnected = (data: any) => {
-            if (!data || !data.player) {
-                return;
-            }
+    const onInitialDataLoaded = (data: any) => loadGame(data);
+    const onPlayersMovement = (data: any) => {
+      if (!data || !data.players) {
+        return;
+      }
 
-            const anotherPlayer = data.player;
-            const summonerId = data.summonerId;
-            if (!playersMap.has(summonerId)) {
-                playersMap.set(summonerId, new Character(gameContext, anotherPlayer.characterImage, characterPlaceholderImg, socketInstance, anotherPlayer.x, anotherPlayer.y, localization));
-            }
-        };
+      data.players.forEach((player: any) => {
+        if (playersMap.has(player.summonerId)) {
+          let playerData = playersMap.get(player.summonerId);
+          if (!playerData) {
+            return;
+          }
+        }
+      });
+    };
+    const onAnotherPlayerConnected = (data: any) => {
+      if (!data || !data.player) {
+        return;
+      }
 
-        const onAnotherPlayerMoved = (data: any) => {
-            if (!data || !data.anotherPlayer) {
-                return;
-            }
+      const anotherPlayer = data.player;
+      const summonerId = data.summonerId;
+      if (!playersMap.has(summonerId)) {
+        playersCount.value += 1;
+        playersMap.set(summonerId, new Character(gameContext, anotherPlayer.characterImage, characterPlaceholderImg, socketInstance, anotherPlayer.x, anotherPlayer.y, localization));
+      }
+    };
 
-            const anotherPlayer = data.anotherPlayer;
+    const onAnotherPlayerMoved = (data: any) => {
+      if (!data || !data.anotherPlayer) {
+        return;
+      }
 
-            const anotherPlayerData = playersMap.get(anotherPlayer.summonerId);
-            if (!anotherPlayerData) {
-                return;
-            }
+      const anotherPlayer = data.anotherPlayer;
 
-            anotherPlayerData.setPositionX(anotherPlayer.x);
-            anotherPlayerData.setPositionY(anotherPlayer.y);
-        };
+      const anotherPlayerData = playersMap.get(anotherPlayer.summonerId);
+      if (!anotherPlayerData) {
+        return;
+      }
 
-        const onAnotherPlayerDisconnected = (data: any) => {
-            if (!data || !data.summonerId) {
-                return;
-            }
+      anotherPlayerData.setPositionX(anotherPlayer.x);
+      anotherPlayerData.setPositionY(anotherPlayer.y);
+    };
 
-            playersMap.delete(data.summonerId);
-        };
+    const onAnotherPlayerDisconnected = (data: any) => {
+      if (!data || !data.summonerId) {
+        return;
+      }
 
-        const gameLoop = (data: any) => {
-            gameContext.clearRect(0, 0, GAME_CONFIG.width, GAME_CONFIG.height);
-            localization.drawBackground();
-            character.animationFrame();
-            playersMap.forEach((character: Character) => character.animationFrame());
-            window.requestAnimationFrame(() => gameLoop(data));
-        };
+      playersCount.value -= 1;
+      playersMap.delete(data.summonerId);
+    };
 
-        const loadGame = async (data: any) => {
-            const characterImage = data.summoner.characterImage;
-            gameContext = prepareAndGetContext(gameBoardRef.value);
-            await loadBackground(data.location);
-            loadingProgress.value = 25;
-            // loadingStage.value = 1;
-            // drawBackground(data.backgroundImage);
-            characterPlaceholderImg = await loadImage('characters/Sprite-character-placeholder.png');
-            character = new MainCharacter(gameContext, characterImage, characterPlaceholderImg, socketInstance, data.summoner.x, data.summoner.y, localization);
-            loadingProgress.value = 50;
-            // loadingStage.value = 2;
-            data.players.forEach((player: any) => playersMap.set(player.summonerId, new Character(
-                gameContext, player.characterImage, characterPlaceholderImg, socketInstance, player.x, player.y, localization
-            )));
+    const gameLoop = (data: any) => {
+      gameContext.clearRect(0, 0, GAME_CONFIG.width, GAME_CONFIG.height);
+      localization.drawBackground();
+      character.animationFrame();
+      playersMap.forEach((character: Character) => character.animationFrame());
+      window.requestAnimationFrame(() => gameLoop(data));
+    };
 
-            // drawCharacter(data.characterImage, data.xPos, data.yPos);
-            loadingProgress.value = 75;
-            // loadingStage.value = 3;
-            loadingProgress.value = 100;
-            // loadingStage.value = 4;
-            isLoadingState.value = false;
+    const loadGame = async (data: any) => {
+      const characterImage = data.summoner.characterImage;
+      gameContext = prepareAndGetContext(gameBoardRef.value);
+      await loadBackground(data.location);
+      loadingProgress.value = 25;
+      // loadingStage.value = 1;
+      // drawBackground(data.backgroundImage);
+      characterPlaceholderImg = await loadImage('characters/Sprite-character-placeholder.png');
+      character = new MainCharacter(gameContext, characterImage, characterPlaceholderImg, socketInstance, data.summoner.x, data.summoner.y, localization);
+      loadingProgress.value = 50;
+      // loadingStage.value = 2;
 
-            window.requestAnimationFrame(() => gameLoop(data));
-        };
+      playersCount.value = data.players.length + 1;
+      data.players.forEach((player: any) => playersMap.set(player.summonerId, new Character(
+          gameContext, player.characterImage, characterPlaceholderImg, socketInstance, player.x, player.y, localization
+      )));
 
-        const loadBackground = async (location: any): Promise<void> => {
-            bgImage = await loadImage(location.backgroundImage);
-            localization = new Localization(bgImage, gameContext, location.width, location.height);
-        };
+      // drawCharacter(data.characterImage, data.xPos, data.yPos);
+      loadingProgress.value = 75;
+      // loadingStage.value = 3;
+      loadingProgress.value = 100;
+      // loadingStage.value = 4;
+      isLoadingState.value = false;
 
-        return {
-            GAME_CONFIG,
-            gameBoardRef,
-            isLoadingState,
-            loadingStage,
-            loadingProgress,
-            onAnotherPlayerConnected,
-            onAnotherPlayerDisconnected,
-            onAnotherPlayerMoved,
-            onInitialDataLoaded,
-            onPlayersMovement
-        };
-    }
+      window.requestAnimationFrame(() => gameLoop(data));
+    };
+
+    const loadBackground = async (location: any): Promise<void> => {
+      bgImage = await loadImage(location.backgroundImage);
+      localization = new Localization(bgImage, gameContext, location.width, location.height);
+    };
+
+    return {
+      GAME_CONFIG,
+      gameBoardRef,
+      isLoadingState,
+      loadingStage,
+      loadingProgress,
+      playersCount,
+      onAnotherPlayerConnected,
+      onAnotherPlayerDisconnected,
+      onAnotherPlayerMoved,
+      onInitialDataLoaded,
+      onPlayersMovement
+    };
+  }
 }
 </script>
 <style lang="scss">
@@ -158,6 +168,7 @@ export default {
     background-color: $color-background;
     display: flex;
     justify-content: center;
+    flex-direction: column;
     align-items: center;
     height: 100vh;
   }
