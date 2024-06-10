3102d44721569f5d7064e85021306141a7ea6e57
Marcin Radwan <marcin.radwan@gate-software.com>
2024-06-10 09:45:55 +0200

    fix: directory structure for ts-node-dev

--- game/server/src/index.ts
+++ game/server/src/index.ts
@@ -1,36 +1,28 @@
-// asddsadsa
+import "reflect-metadata";
+import {initDbConnection} from "./shared/database/connection/set-connection";
+
+const serverMode: string = process.env.SERVER_MODE || 'socket-io';
+const serverPort: string = (!!process.env && !!process.env.SERVER_PORT) ? process.env.SERVER_PORT : '8888';
+
+import http = require('http');
+import express = require('express');
+import cors = require('cors');
+import bodyParser = require('body-parser');
+
+const app = express();
 
 
+app.use(bodyParser.json());
+app.use(cors({
+    origin: '*'
+}));
 
-// // import "reflect-metadata";
-// import {initDbConnection} from "./shared/database/connection/set-connection";
-//
-// const serverMode: string = process.env.SERVER_MODE || 'socket-io';
-// const serverPort: string = (!!process.env && !!process.env.SERVER_PORT) ? process.env.SERVER_PORT : '8888';
-//
-// // @ts-ignore
-// import http from 'http';
-// // @ts-ignore
-// import express from 'express';
-// // @ts-ignore
-// import cors from 'cors';
-// // @ts-ignore
-// import bodyParser from 'body-parser';
-//
-// const app = express();
-//
-//
-// app.use(bodyParser.json());
-// app.use(cors({
-//     origin: '*'
-// }));
-//
-// const httpServer = http.createServer(app);
-//
-// const startServerInstance = async () => {
-//     // await initDbConnection();
-//     // const socket = serverMode === 'socket-io' ? require('./socket-io/socket-io').SocketIOServer : require('./sock-js/sock-js').SockJSServer;
-//     // const socketInstance = new socket(httpServer, app);
-//     // socketInstance.start(Number(serverPort));
-// };
-// startServerInstance().then(() => console.log('Server started'));
+const httpServer = http.createServer(app);
+
+const startServerInstance = async () => {
+    await initDbConnection();
+    const socket = serverMode === 'socket-io' ? require('./socket-io/socket-io').SocketIOServer : require('./sock-js/sock-js').SockJSServer;
+    const socketInstance = new socket(httpServer, app);
+    socketInstance.start(Number(serverPort));
+};
+startServerInstance().then(() => console.log('Server started'));
