7760250642a7183d31fb30c28a5fe11a7f7f55dc
Marcin Radwan <marcin.radwan08@gmail.com>
2024-06-10 20:22:59 +0200

    feat: added mocked links

--- game/client/src/pages/home/components/login/LoginForm.vue
+++ game/client/src/pages/home/components/login/LoginForm.vue
@@ -13,13 +13,18 @@
                  class="input-text__input input-text__input--icon"
                  id="password"
                  v-model="passwordInput">
-          <img :src="require('@/assets/icons/' + (showPassword ? 'icon-eye-disabled-24x24.svg' : 'icon-eye-brown-24x24.svg'))"
-               @click="togglePwdVisibility"
-               class="input-text__icon input-text__icon--btn"
-               role="button"
-               alt="Icon eye"/>
+          <img
+              :src="require('@/assets/icons/' + (showPassword ? 'icon-eye-disabled-24x24.svg' : 'icon-eye-brown-24x24.svg'))"
+              @click="togglePwdVisibility"
+              class="input-text__icon input-text__icon--btn"
+              role="button"
+              alt="Icon eye"/>
         </div>
         <SimpleButton text="Login" @click="login"/>
+        <div class="login-form__extra-operations">
+          <SimpleLink href="/" text="Remind password"/>
+          <SimpleLink href="/" text="Register"/>
+        </div>
       </form>
     </div>
   </div>
@@ -31,11 +36,15 @@ import {IAuthLoginResponse} from "@/interfaces/api/auth.interface";
 import {saveIntoLocalStorage} from "@/pages/game/helpers/local-storage.helper";
 import {LocalStorageKeyEnum} from "@/enums/local-storage-key.enum";
 import SimpleButton from "@/pages/shared/components/buttons/SimpleButton.vue";
+import SimpleLink from "@/pages/shared/components/links/SimpleLink.vue";
 import {LoaderService} from "@/services/loader.service";
 
 export default {
   name: 'LoginForm',
-  components: {SimpleButton},
+  components: {
+    SimpleButton,
+    SimpleLink
+  },
   emits: ['login-error', 'login-success'],
   setup(props: any, context: any) {
     const loginInput = ref('');
@@ -78,7 +87,14 @@ export default {
   }
 
   &__login-btn {
+    margin-top: 0.25rem;
+  }
+
+  &__extra-operations {
     margin-top: 1rem;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
   }
 }
 </style>
---
+++ game/client/src/pages/shared/components/links/SimpleLink.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="link__wrapper">
+    <a :href="href"
+       class="link__btn"
+       @click="onClick($event)">
+      {{ text }}
+    </a>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+    href: {
+      type: String,
+      required: true
+    }
+  },
+  emits: ['click'],
+  setup(props: any, context: any) {
+    const onClick = (event: any) => {
+      event.preventDefault();
+      context.emit('click');
+    };
+
+    return {onClick};
+  }
+}
+</script>
+<style lang="scss" scoped>
+@import "../../../../assets/styles/components/page/links";
+</style>
